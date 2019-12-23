001d412af9ceb5c6e74fa11d4d27d8f37e7e419b
Olivier Barais <olivier.barais@gmail.com>
2019-12-23 11:29:36 +0100

    update

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 # hlsserver-oid
+
+
+curl -X POST \
+  http://localhost:8080/auth/realms/demo/protocol/openid-connect/token \
+  -d 'username=user&password=password&grant_type=urn:ietf:params:oauth:grant-type:uma-ticket&client_id=nodejs-apiserverweb'
+
+
+curl -X POST \
+    -H "Content-Type: application/x-www-form-urlencoded" \
+    -d 'grant_type=client_credentials&client_id=nodejsdemo&client_secret=6d44fd6e-982f-4e9d-9d18-ef95cd064bc2' \
+    "http://localhost:8080/auth/realms/demo/protocol/openid-connect/token"
+
+ curl \
+  -d "client_id=nodejsdemoweb" \
+  -d "username=user" \
+  -d "password=password" \
+  -d "grant_type=password" \
+  "http://localhost:8080/auth/realms/demo/protocol/openid-connect/token" | jq -r '.access_token'
+  
--- app2.js
+++ app2.js
@@ -1,17 +1,62 @@
 
-const express = require('express')
+
+import express from 'express';
+import bodyParser from 'body-parser';
+// 1
+import session from 'express-session';
+import Keycloak from 'keycloak-connect';
+
 const app = express()
 
+app.use(bodyParser.json());
 
-app.use('/', express.static('public/videos'));
+// 2
+const memoryStore = new session.MemoryStore();
 
-app.get('/player.html', function (req, res) {
-    var s = `<html><head><title>HLS Player fed by node.js' +
+app.use(
+  session({
+    secret: '6d44fd6e-982f-4e9d-9d18-ef95cd064bc2',
+    resave: false,
+    saveUninitialized: true,
+    store: memoryStore
+  })
+);
+
+// 3
+const keycloak = new Keycloak({
+  store: memoryStore
+});
+
+app.use(
+  keycloak.middleware({
+    logout: '/logout',
+    admin: '/'
+  })
+);
+
+app.get('/api/unsecured', function(req, res) {
+  res.json({ message: 'This is an unsecured endpoint payload' });
+});
+
+app.get('/api/user', keycloak.protect(), function(req, res) {
+  res.json({ message: 'This is an USER endpoint payload' });
+});
+app.get('/api/admin', keycloak.protect('realm:admin'), function(req, res) {
+  res.json({ message: 'This is an ADMIN endpoint payload' });
+});
+
+
+app.use('/', keycloak.protect(), express.static('public/videos'));
+
+app.get('/index.html', keycloak.protect(), function (req, res) {
+  var s = `<html><head><title>HLS Player fed by node.js' +
         '</title></head>
         <script src="https://cdn.jsdelivr.net/npm/hls.js@latest"></script>
         
         <body>
-        <video id="video" controls autoplay></video>        
+        <video id="video" controls autoplay></video>   
+        <BR>
+        <a href="/logout"> logout</a>     
         <script>
   var video = document.getElementById('video');
   if(Hls.isSupported()) {
@@ -30,14 +75,12 @@ app.get('/player.html', function (req, res) {
   }
 </script>
         </body></html>`
-        res.send(s);
-  });
-  
+  res.send(s);
+});
+
 
 
 
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!')
 })
-
-
