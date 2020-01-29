537224bc060279ed617cd5aecc8d494bfc64dfa0
Olivier Barais <olivier.barais@gmail.com>
2020-01-29 18:00:06 +0100

    update

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 var data = require('./keycloak1.json');
 //Manipulate data
 delete data['auth-server-url'];
-data['auth-server-url'] = 'http://' + process.env.KEYCLOAKIP + ':8080/auth';
+data['auth-server-url'] = 'http://' + process.env.KEYCLOAKIPRESOLVE + ':8080/auth';
 //Output data
 console.log(JSON.stringify(data));
